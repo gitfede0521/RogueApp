acae92af66296429c391f481b856586b164f4e9d
Federico Soavi <federico.soavi@sassi.it>
2018-08-03 11:19:39 +0200

    Add project files.

---
+++ RogueApp/Engine.cs
@@ -0,0 +1,72 @@
+﻿using RLNET;
+using System.Windows.Forms;
+
+
+public class Engine
+{
+    private RLRootConsole rootConsole;
+    private int playerX;
+    private int playerY;
+    public static Keys ModifierKeys { get; }
+
+    bool isFullscreen = false;
+
+    public Engine(RLRootConsole console)
+    {
+        rootConsole = console;
+
+        playerY = rootConsole.Height / 2;
+        playerX = rootConsole.Width / 2;
+
+        rootConsole.Render += Render;
+        rootConsole.Update += Update;
+    }
+
+    public void Render(object sender, UpdateEventArgs e)
+    {
+        rootConsole.Clear();
+        rootConsole.Set(playerX, playerY, RLColor.White, null, '@');
+        rootConsole.Draw();
+    }
+
+    public void Update(object sender, UpdateEventArgs e)
+    {
+        RLKeyPress keyPress = rootConsole.Keyboard.GetKeyPress();
+        RLMouse rLMouse = rootConsole.Mouse;
+
+
+        if (keyPress != null)
+        {
+            switch (keyPress.Key)
+            {
+                case RLKey.Up: playerY--; break;
+                case RLKey.Down: playerY++; break;
+                case RLKey.Left: playerX--; break;
+                case RLKey.Right: playerX++; break;
+
+                case RLKey.Escape: rootConsole.Close(); break; 
+            }
+
+            if(keyPress.Alt)
+            {
+                switch (keyPress.Key)
+                {
+                    case RLKey.Enter: ToggleFullscreen(); break;
+                }
+            }
+
+        }
+    }
+    public void ToggleFullscreen()
+    {
+        if (isFullscreen)
+        {
+            rootConsole.SetWindowState(RLWindowState.Normal);
+        }
+        else
+        {
+            rootConsole.SetWindowState(RLWindowState.Fullscreen);
+        }
+        isFullscreen = !isFullscreen;
+    }
+}
---
+++ RogueApp/Game.cs
@@ -0,0 +1,18 @@
+﻿using RLNET;
+using System;
+
+namespace RLNET_Tutorial
+{
+    public class Game
+    {
+        public static RLRootConsole rootConsole;
+        private static Engine engine;
+
+        public static void Main()
+        {
+            rootConsole = new RLRootConsole("ascii_8x8.png", 80, 50, 8, 8,title:"GAME");
+            engine = new Engine(rootConsole);
+            rootConsole.Run();
+        }
+    }
+}
